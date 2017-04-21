1077e054cd21aff7cbb1f9fb7c56348754c0414b
Patryk Bielecki <p.patrykbielecki@gmail.com>
2017-04-21 14:08:01 +0200

    0.2

---
+++ TrainFinder.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrainFinder.Domain.Abstract;
+using TrainFinder.Domain.Entities;
+
+namespace TrainFinder.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ITrainRepository repository;
+        public AdminController(ITrainRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Trains);
+        }
+
+        public ViewResult Edit(int trainID)
+        {
+            Train train = repository.Trains.FirstOrDefault(p => p.TrainID == trainID);
+            return View(train);
+        }
+    }
+}
